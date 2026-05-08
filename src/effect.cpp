7a90dee4a1cdfe3213bb72934c38a9c2b89dd386
xurei <xureilab@gmail.com>
2026-05-08 20:21:03 +0200

    🐛 Fixed oversimplification in commit 782dab7d5be8b0e5ec6dca1fddbb86203262071a

--- src/effect.cpp
+++ src/effect.cpp
@@ -206,7 +206,7 @@ void shadertastic_effect_t::set_step_params(int current_step, gs_texture_t *inte
 
 
 void shadertastic_effect_t::render_shader(uint32_t cx, uint32_t cy, bool use_linear) const {
-    const char *tech_name = gs_get_color_space() == GS_CS_SRGB ? "Draw" : "DrawLinear";
+    const char *tech_name = (!use_linear && gs_get_color_space() == GS_CS_SRGB) ? "Draw" : "DrawLinear";
     while (main_shader->loop(tech_name)) {
         gs_draw_sprite(nullptr, 0, cx, cy);
     }
